3e78f0b1fbe43522546b55e4f0a508761a7ff6cb
alokgupta001 <alokgupta011005@gmail.com>
2026-05-07 11:59:09 +0530

    Time: 106 ms (5.27%) | Memory: 89.8 MB (5.12%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -8,16 +8,28 @@ class Solution {
             }
         }
         helper(0,s,new ArrayList<>());
+        
         return res;
 
     }
+    List<String> getSubString(List<String> temp,String s){
+        List<String> ans = new ArrayList<>();
+        for(String t:temp){
+            String [] part = t.split(" ");
+            int i = Integer.parseInt(part[0]);
+            int j = Integer.parseInt(part[1]);
+            ans.add(s.substring(i,j));
+        }
+        return ans;
+    }
     void helper(int start,String s,List<String> temp){
         if(start == s.length()){
-            res.add(new ArrayList<>(temp));
+            List<String> ans = getSubString(temp,s);
+            res.add(new ArrayList<>(ans));
             return;
         }
         for(int i=start;i<s.length();i++){
-            String part=s.substring(start,i+1);
+            String part= start+" "+(i+1);
             if(dp[start][i]!=null && dp[start][i]==true){
                 temp.add(part);
                 helper(i+1,s,temp);
