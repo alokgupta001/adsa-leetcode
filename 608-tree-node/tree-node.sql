f85c24386acd6a1838bf234f7b534c59e7ac8b54
alokgupta001 <alokgupta011005@gmail.com>
2026-06-02 15:11:46 +0530

    Time: 520 ms (26.02%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,9 +1,4 @@
-SELECT id,
-
-    CASE 
-        WHEN p_id IS NULL THEN 'Root'
-        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
-        ELSE 'Leaf'
-        END AS type
- FROM Tree
+with recursive cte as(select id, p_id from Tree where p_id is null
+union all
+select t.id,t.p_id from tree t join cte  c on t.p_id=c.id) select id ,case when p_id is null then"Root" when id in (select p_id from tree where p_id is not null) then "Inner" else "Leaf" end as type from cte order by id;
 		
