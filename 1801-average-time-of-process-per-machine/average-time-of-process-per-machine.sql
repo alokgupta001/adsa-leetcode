e4de5d20b5e7995639eb9f5b99bbebebc716c810
alokgupta001 <alokgupta011005@gmail.com>
2026-07-22 23:40:48 +0530

    Time: 251 ms (67.39%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,6 +1,5 @@
-select a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time 
-from Activity a1
-join Activity a2 
-on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
-and a1.activity_type='start' and a2.activity_type='end'
-group by a1.machine_id
+# Write your MySQL query statement below
+with cte as(
+    select t1.machine_id, t2.timestamp - t1.timestamp as timetaken from Activity t1 join Activity t2 on t1.process_id = t2.process_id and t1.machine_id = t2.machine_id and t1.activity_type = 'start' and t2.activity_type= 'end'
+)
+select machine_id , round(sum(timetaken)/count(*),3) as processing_time from cte group by machine_id;
