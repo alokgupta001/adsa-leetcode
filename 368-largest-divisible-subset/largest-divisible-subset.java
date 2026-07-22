7eeaf6db3f180ac5b3af44a2f679315405be8b46
alokgupta001 <alokgupta011005@gmail.com>
2026-07-22 21:39:51 +0530

    Time: 16 ms (99.86%) | Memory: 45.2 MB (58.44%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -1,30 +1,31 @@
 class Solution {
-    List<Integer> [] memo;
     public List<Integer> largestDivisibleSubset(int[] nums) {
         Arrays.sort(nums);
-        List<Integer> ans = new ArrayList<>();
-        memo = new ArrayList[nums.length];
-        for(int i = 0;i<nums.length;i++){
-            List<Integer> curr = solve(nums,i);
-            if(curr.size()>ans.size()){
-                ans = new ArrayList<>(curr);
-            }
-        }
-        return ans;
-    }
-    List<Integer> solve (int []nums,int i){
-        if(memo[i]!=null) return memo[i];
-        List<Integer> best = new ArrayList<>();
-        best.add(nums[i]);
-        for(int j=0;j<i;j++){
-            if(nums[i]%nums[j]==0){
-                List<Integer> curr = solve(nums,j);
-                if(1+curr.size()>best.size()){
-                    best = new ArrayList<>(curr);
-                    best.add(nums[i]);
+        int [] dp = new int[nums.length];
+        int [] hash = new int[nums.length];
+        Arrays.fill(dp,1);
+        Arrays.fill(hash,-1);
+        int maxi=1;
+        int last=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<1+dp[j]){
+                    dp[i] = 1+dp[j];
+                    hash[i] = j;
                 }
             }
+            if(dp[i]>maxi){
+                maxi=dp[i];
+                last=i;
+            }
         }
-        return memo[i]= best;
+        List<Integer> ans = new ArrayList<>();
+        
+        while(last!=-1){
+            ans.add(nums[last]);
+            last = hash[last];
+        }
+        Collections.reverse(ans);
+        return ans;
     }
 }
