a81ee4d10bcd4e254817227bc52f633fd42a6526
alokgupta001 <alokgupta011005@gmail.com>
2026-07-23 18:30:35 +0530

    Time: 1018 ms (47.92%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,7 @@
 # Write your MySQL query statement below
-WITH total_users AS (
-    SELECT COUNT(*) AS total
-    FROM Users
-)
-SELECT
-    contest_id,
-    ROUND(COUNT(DISTINCT user_id) * 100 / total, 2) AS percentage
-FROM Register
-CROSS JOIN total_users
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id;
+select 
+contest_id, 
+round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as percentage
+from  Register
+group by contest_id
+order by percentage desc,contest_id
